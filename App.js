8db18133f656840136cae172f3f15eb59a93f152
arvind11kumar <71036209+arvind11kumar@users.noreply.github.com>
2022-07-24 00:42:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,94 @@
+
+import './App.css';
+import ButtonAction from './components/ButtonAction';
+//import DreamProject from './components/DreamProject';
+// import ChangevalueonClick from './component/ChangevalueonClick';
+// import Heading from './components/Heading';
+// import InputQuery from './components/InputQuery';
+// import SubHeading from './components/SubHeading';
+// import SubmitButton from './components/SubmitButton';
+//import ContextConsumer from './component/ContextConsumer';
+//import HOC from './component/HOC';
+//import Fetchdata from './component/Fetchdata';
+//import TextCompare from './SmallProject/TextCompare';
+//import App14 from './TextNS/App14';
+//import React12 from './Interview/React12';
+//import LifeCycle from './component/LifeCycle';
+//import Test10 from './TextNS/Test10';
+//import FetchUrl from './component/FetchUrl';
+//import UseEffectText from './component/UseEffectText';
+//import UseEffect from './component/UseEffectText';
+//import FunCount from './component/FunCount';
+// import Greet from './component/Greet';
+// import GreetClass from './component/GreetClass';
+// import CounterFun from './component/CounterFun';
+// import CounterClass from './component/CounterClass';
+// import ThankuforSubcribing from './component/ThankuforSubcribing';
+// import Message from './component/Message';
+// import Destractring from './component/Destractring';
+// import DestractringClass from './component/DestractringClass';
+// import FunctionClick from './component/FunctionClick';
+// import ClassClick from './ClassClick';
+// import EventHandle from './EventHandle';
+// import Example from './component/Example';
+
+//import FetchingData from "./SmallProject/FetchingData";
+//import Tolist from './SmallProject/Tolist';
+
+//import ComponentA from './multipleComp/ComponentA';
+//import Calculater from './SmallProject/Calculater';
+//import TodoLis from './SmallProject/TodoList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+    
+       <ButtonAction/>  
+        {/* <FunctionClick/>
+ <TextCompare/>
+<App14/>  fetching data and display in list
+<ContextConsumer/>
+     <FetchingData/>
+
+
+ <DreamProject/>
+      <Heading/>
+      <SubHeading/>
+      <InputQuery/>
+      <SubmitButton/>
+  <HOC/>
+  <Tolist/>
+   <ChangevalueonClick/>
+         <TodoLis/>
+         <React12/>
+          <UseEffectText/>
+         <LifeCycle/> // <Test10/>
+         <FetchUrl/>
+         <TodoLis/>
+         <Calculater/>
+        <ClassClick/>
+        <EventHandle/>
+        <Example/> 
+ <ComponentA/>
+        <DestractringClass name="radha" heroName="kushwaha"/>
+        <rendringHere/>
+        <FunCount/>
+       <Destractring name="arvind" heroName="kushwaha"/>
+       <Message/>
+       <Greet name="arvind fun" heroName="SuperMan"/>
+       <Greet name="mukesh fun" heroName="batman"/>
+       <GreetClass name="arvind class" heroName="SuperMan"/>
+       <GreetClass name="mukesh class" heroName="batman"/>
+       <ThankuforSubcribing/>
+       <CounterFun/>
+       <CounterClass/> */}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ SubmitButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const SubmitButton=()=>{
+    return(
+        <>
+        <h1>SubmitButton</h1>
+        </>
+    )
+}
+export default SubmitButton;
